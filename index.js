3f94855384c759940e70570343abeb8d75265bb5
Arjun Yonjan <arjun.yonzan@gmail.com>
2021-10-12 11:26:06 +0545

    start...

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+});
+
+const port = process.env.PORT || 4000
+
+app.listen(port, () => console.log(`server running at ${port}`));
